b48f8e13bea523688271727b70cfe2904a97ece7
ChengGuang-Li <cristianoronando777@gmail.com>
2024-03-15 21:51:33 +1100

    rotating island

--- src/models/Latios.jsx
+++ src/models/Latios.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import { useGLTF } from '@react-three/drei'
-import latios from "../assets/3d/latios.glb"
+import { useEffect, useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import latios from "../assets/3d/latios.glb";
 
-const Latios = ({isRotate, ...props}) => {
-    const {scene,animation} = useGLTF(latios);
-    
+const Latios = ({ isRotate, ...props }) => {
+  const ref = useRef();
+  const { scene, animations } = useGLTF(latios);
+  const { actions } = useAnimations(animations, ref);
+  
+  useEffect(() => {
+    if (isRotate && actions["GltfAnimation 0"]) {
+      actions["GltfAnimation 0"].play();
+    } else if (!isRotate && actions["GltfAnimation 0"]) {
+      actions["GltfAnimation 0"].stop();
+    }
+  }, [isRotate, actions]);
+  
   return (
-    <mesh {...props }>
-        <primitive object={scene} />
+    <mesh {...props} ref={ref}>
+      <primitive object={scene} />
     </mesh>
-  )
-}
+  );
+};
 
-export default Latios
+export default Latios;
